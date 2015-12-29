16f136f26d6ad4014bf6f5107010cdea72b94bb0
Chiedo <chiedo@chie.do>
2015-12-29 10:24:46 -0500

    Test progress with error

--- tests.js
+++ tests.js
@@ -4,10 +4,36 @@ import { expect } from 'chai';
 import ob from './src/objob';
 
 describe('Objob', () => {
-  it('should do something', (done) => {
-    expect(true).to.equal(true);
+  let ob1;
+  let ob2;
+  let obArr1;
+
+  before((done) => {
+    ob1 = {
+      name: 'Bob',
+      age: 22,
+      weight: 170,
+    };
+
+    ob2 = {
+      name: 'Bob',
+      feet: 5,
+      weight: 170,
+    };
+
+    obbArr1 = [ob1, ob2];
+
     done();
   });
+
+  describe('keys', () => {
+    it('should return all keys for an object', (done) => {
+      expect(ob(ob1).keys()).to.equal(['name','age', 'weight']);
+      done();
+    });
+    it('should return all keys for an array', (done) => {
+      expect(ob(obArr1).keys()).to.equal(['name','age', 'weight']);
+      done();
+    });
+  });
 });
-
-
