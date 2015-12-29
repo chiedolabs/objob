ad13bbdf98981ce89fd54aed009e9a4a0f4c0bfb
Chiedo <chiedo@chie.do>
2015-12-29 10:16:05 -0500

    Untested progress

--- src/objob.js
+++ src/objob.js
@@ -1,4 +1,30 @@
-'use script';
+'use strict';
+let type = function(x) {
+  if( Object.prototype.toString.call(x) === '[object Array]' ) {
+    return 'array';
+  } else if(typeof x === 'object') {
+    return 'object';
+  }
+};
+
+Array.prototype.getUnique = function (createArray) {
+  createArray = createArray === true ? true : false;
+  let temp = JSON.stringify(this);
+  temp = JSON.parse(temp);
+  if (createArray) {
+    let unique = temp.filter((elem, pos) => {
+      return temp.indexOf(elem) === pos;
+    });
+    return unique;
+  }
+  else {
+    let unique = this.filter((elem, pos) => {
+      return this.indexOf(elem) === pos;
+    });
+    this.length = 0;
+    this.splice(0, 0, unique);
+  }
+};
 
 /**
  * Returns an objob object
@@ -7,6 +33,34 @@
  * @returns {(object|object[])}
  */
 let ob = function (x) {
+  if(type(x) !== 'array' && type(x) !== 'object') {
+    return undefined;
+  }
+
+  return {
+    keys:() => {
+      let keys = [];
+
+      if(type(x) === 'array') {
+        for(let i = 0; i < x.length; x++){
+          for(let k in x) {
+            keys.push(k);
+          }
+        }
+      } else if(type(x) === 'object') {
+        for(let k in x) {
+          keys.push(k);
+        };
+      }
+      return keys.getUnique();
+    },
+    many: () => {
+    },
+    with: () => {
+    },
+    without:() => {
+    },
+  };
 };
 
 export default ob;
