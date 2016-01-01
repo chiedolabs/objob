05ac5c104874c8e41fbcce2f6a8e011fdfbad046
Chiedo <chiedo@chie.do>
2016-01-01 15:56:20 -0500

    Refactored implementation

--- src/objob.js
+++ src/objob.js
@@ -6,278 +6,268 @@ import contains from 'string-contains';
 import merge from 'deepmerge';
 import { makeFlattenedShallow } from './functions';
 
-/**
- * Returns an objob object
- *
- * @param {(object|object[])} subject
- * @returns {(object|object[])}
- */
-let ob = function (subject) {
+let ob = {
+  /*
+   *  Returns an object without the given keys.
+   *
+   *  @param {string[]} keys
+   *  @returns {object}
+   */
+  deselect: function(subject, keys = []){
+    let allKeys = ob.keys(ob.flatten(subject));
+    let keysToKeep = [];
 
-  return {
-    /*
-     *  Returns an object without the given keys.
-     *
-     *  @param {string[]} keys
-     *  @returns {object}
-     */
-    deselect: function(keys = []){
-      let allKeys = ob(ob(subject).flatten()).keys();
-      let keysToKeep = [];
+    for( let subjectKey of allKeys ) {
+      let keepKey = true;
 
-      for( let subjectKey of allKeys ) {
-        let keepKey = true;
-
-        for( let keyToRemove of keys ){
-          if(subjectKey === keyToRemove){
-            keepKey = false;
-          }
-        }
-
-        if(keepKey){
-          keysToKeep.push(subjectKey);
+      for( let keyToRemove of keys ){
+        if(subjectKey === keyToRemove){
+          keepKey = false;
         }
       }
 
-      return ob(subject).select(keysToKeep);
-    },
-    /*
-     *  Takes a flattened object or array and expands it back to a full object
-     *  or array.
-     *
-     *  @returns {object|object[]}
-     */
-    expand: function(depth = 1){
-      let res;
-      subject = makeFlattenedShallow(subject);
-
-      // Determine if an array is represented by the flattened object
-      let rootObjectPresent = true;
-      if(depth === 1) {
-        rootObjectPresent = false;
-        for(let key in subject) {
-          let rootArrayPresent = key.match(/^\d/ig);
-
-          rootObjectPresent = (rootObjectPresent || !rootArrayPresent);
-        }
+      if(keepKey){
+        keysToKeep.push(subjectKey);
       }
+    }
 
-      if(rootObjectPresent === false && depth === 1) {
-        res = [];
-        for(let key in subject) {
-          res.push(subject[key]);
-        }
-      } else {
-        let keyChains =  ob(subject).keys();
+    return ob.select(subject, keysToKeep);
+  },
+  /*
+   *  Takes a flattened object or array and expands it back to a full object
+   *  or array.
+   *
+   *  @returns {object|object[]}
+   */
+  expand: function(subject, depth = 1){
+    let res;
+    subject = makeFlattenedShallow(subject);
 
-        // When the object is just {'example.example': y}
-        // One key and one value
-        if(keyChains.length === 1) {
-          let tmp = {};
-          let keyChain = keyChains[0]; // something like 'first.another.another'
-          let value = subject[keyChain];
-          let count;
+    // Determine if an array is represented by the flattened object
+    let rootObjectPresent = true;
+    if(depth === 1) {
+      rootObjectPresent = false;
+      for(let key in subject) {
+        let rootArrayPresent = key.match(/^\d/ig);
 
-          res = tmp; // Poining to tmp so that we have a place holder before nesting
-          count = 1;
-          let keys = keyChain.split('.');
-          for(let key of keys) {
-            if(count === keys.length) {
-              tmp[key] = value;
+        rootObjectPresent = (rootObjectPresent || !rootArrayPresent);
+      }
+    }
+
+    if(rootObjectPresent === false && depth === 1) {
+      res = [];
+      for(let key in subject) {
+        res.push(subject[key]);
+      }
+    } else {
+      let keyChains =  ob.keys(subject);
+
+      // When the object is just {'example.example': y}
+      // One key and one value
+      if(keyChains.length === 1) {
+        let tmp = {};
+        let keyChain = keyChains[0]; // something like 'first.another.another'
+        let value = subject[keyChain];
+        let count;
+
+        res = tmp; // Poining to tmp so that we have a place holder before nesting
+        count = 1;
+        let keys = keyChain.split('.');
+        for(let key of keys) {
+          if(count === keys.length) {
+            tmp[key] = value;
+          } else {
+            let isArray = contains(key, '[]');
+            if(isArray) {
+              key = key.replace('[]','');
+              tmp[key] = [];
             } else {
-              let isArray = contains(key, '[]');
-              if(isArray) {
-                key = key.replace('[]','');
-                tmp[key] = [];
-              } else {
-                tmp[key] = {};
-              }
-
-              tmp = tmp[key];
+              tmp[key] = {};
             }
-            count++;
+
+            tmp = tmp[key];
           }
-
-        } else {
-          // If multiple keychains in the object, simplify our logic a bit
-          res = {};
-          for(let i in subject) {
-            let tmp = {};
-            tmp[i] = subject[i];
-            res = merge(res, ob(tmp).expand(++depth));
-          }
-        }
-      }
-      return res;
-      //return ob(res).removeUndefs();
-    },
-    /*
-     *  Takes an object or array and return a flattened representation of that object or array
-     *  that has one level of depth. This allows you to do complex operations on your object
-     *  while it's in a format that's easier to work with.
-     *
-     *  @returns {object|object[]}
-     */
-    flatten: function(prefix='', depth = 1){
-      let res;
-
-      if(type(subject) === 'array' && depth === 1) {
-        res = [];
-
-        for(let i of subject){
-          res = res.concat(ob(i).flatten(prefix, ++depth));
+          count++;
         }
 
-        return res;
       } else {
+        // If multiple keychains in the object, simplify our logic a bit
         res = {};
-
-        if(type(subject) === 'object' || type(subject) === 'array'){
-
-          for(let i in subject) {
-            let tmpPrefix;
-            if(prefix === '') {
-              tmpPrefix = `${i}`;
-            } else {
-              tmpPrefix = `${prefix}.${i}`;
-            }
-
-            if(type(subject[i]) === 'array') {
-              tmpPrefix = tmpPrefix + '[]';
-            }
-
-            res[tmpPrefix] = subject[i];
-
-            res = merge(res, ob(subject[i]).flatten(tmpPrefix, ++depth));
-          }
+        for(let i in subject) {
+          let tmp = {};
+          tmp[i] = subject[i];
+          res = merge(res, ob.expand(tmp, ++depth));
         }
       }
-      return res;
-    },
-    /*
-     * Return all keys for an object or all keys of each object in an array.
-     *
-     *  @returns {string[]}
-     */
-    keys:function() {
-      let keys = [];
+    }
+    return res;
+  },
+  /*
+   *  Takes an object or array and return a flattened representation of that object or array
+   *  that has one level of depth. This allows you to do complex operations on your object
+   *  while it's in a format that's easier to work with.
+   *
+   *  @returns {object|object[]}
+   */
+  flatten: function(subject, prefix='', depth = 1){
+    let res;
 
-      if(type(subject) === 'array') {
-        for(let i of subject){
-          keys = keys.concat(ob(i).keys());
-        }
-      } else {
-        for(let k in subject) {
-          keys.push(k);
-        };
-      }
+    if(type(subject) === 'array' && depth === 1) {
+      res = [];
 
-      return uniques(keys);
-    },
-    /*
-     * Returns many of the object. If an array is used, it will just return
-     * the given array.
-     *
-     *  @returns {object[]}
-     */
-    many: (num = 2) => {
-      let arr = [];
-
-      if(type(subject) === 'array') {
-        return subject;
-      } else {
-        for(let i = 0; i < num; i++){
-          arr.push(subject);
-        }
-      }
-
-      return arr;
-    },
-    /*
-     * Removes all keys with undefined values from an object or array
-     *
-     *  @returns {object|object[]}
-     */
-    removeUndefs: () => {
-      let res;
-
-      if(type(subject) === 'array') {
-        res = [];
-        for(let key in subject) {
-          if(subject[key] === undefined) {
-          } else {
-            res.push(ob(subject[key]).removeUndefs());
-          }
-        }
-      } else if(type(subject) === 'object') {
-        for(let key in subject) {
-          if(subject[key] === undefined) {
-            delete subject[key];
-          } else {
-            subject[key] = ob(subject[key]).removeUndefs();
-          }
-        }
-
-        return subject;
-      } else {
-        return subject;
+      for(let i of subject){
+        res = res.concat(ob.flatten(i, prefix, ++depth));
       }
 
       return res;
-    },
-    /*
-     *  Returns an object only with the given keys.
-     *
-     *  @param {string[]} keys
-     *  @returns {object}
-     */
-    select: (keys = []) => {
-      let resp;
+    } else {
+      res = {};
 
-      if(type(subject) === 'array') {
-        resp = [];
+      if(type(subject) === 'object' || type(subject) === 'array'){
 
-        for(let i of subject){
-          resp = resp.concat(ob(i).select(keys));
+        for(let i in subject) {
+          let tmpPrefix;
+          if(prefix === '') {
+            tmpPrefix = `${i}`;
+          } else {
+            tmpPrefix = `${prefix}.${i}`;
+          }
+
+          if(type(subject[i]) === 'array') {
+            tmpPrefix = tmpPrefix + '[]';
+          }
+
+          res[tmpPrefix] = subject[i];
+
+          res = merge(res, ob.flatten(subject[i],tmpPrefix, ++depth));
         }
-      } else {
-        resp = {};
+      }
+    }
+    return res;
+  },
+  /*
+   * Return all keys for an object or all keys of each object in an array.
+   *
+   *  @returns {string[]}
+   */
+  keys:function(subject) {
+    let keys = [];
 
-        let flat = ob(subject).flatten();
+    if(type(subject) === 'array') {
+      for(let i of subject){
+        keys = keys.concat(ob.keys(i));
+      }
+    } else {
+      for(let k in subject) {
+        keys.push(k);
+      };
+    }
 
-        for (let actualKey in flat){
-          for (let desiredKey of keys){
-            if(actualKey === desiredKey) {
-              resp[actualKey] = flat[actualKey];
-            }
+    return uniques(keys);
+  },
+  /*
+   * Returns many of the object. If an array is used, it will just return
+   * the given array.
+   *
+   *  @returns {object[]}
+   */
+  many: (subject, num = 2) => {
+    let arr = [];
+
+    if(type(subject) === 'array') {
+      return subject;
+    } else {
+      for(let i = 0; i < num; i++){
+        arr.push(subject);
+      }
+    }
+
+    return arr;
+  },
+  /*
+   * Removes all keys with undefined values from an object or array
+   *
+   *  @returns {object|object[]}
+   */
+  removeUndefs: (subject) => {
+    let res;
+
+    if(type(subject) === 'array') {
+      res = [];
+      for(let key in subject) {
+        if(subject[key] === undefined) {
+        } else {
+          res.push(ob.removeUndefs(subject[key]));
+        }
+      }
+    } else if(type(subject) === 'object') {
+      for(let key in subject) {
+        if(subject[key] === undefined) {
+          delete subject[key];
+        } else {
+          subject[key] = ob.removeUndefs(subject[key]);
+        }
+      }
+
+      return subject;
+    } else {
+      return subject;
+    }
+
+    return res;
+  },
+  /*
+   *  Returns an object only with the given keys.
+   *
+   *  @param {string[]} keys
+   *  @returns {object}
+   */
+  select: (subject, keys = []) => {
+    let resp;
+
+    if(type(subject) === 'array') {
+      resp = [];
+
+      for(let i of subject){
+        resp = resp.concat(ob.select(i, keys));
+      }
+    } else {
+      resp = {};
+
+      let flat = ob.flatten(subject);
+
+      for (let actualKey in flat){
+        for (let desiredKey of keys){
+          if(actualKey === desiredKey) {
+            resp[actualKey] = flat[actualKey];
           }
         }
-        resp = ob(resp).expand();
       }
+      resp = ob.expand(resp);
+    }
 
-      return resp;
-    },
-    /*
-     * Returns all values for a given object or array as an array.
-     *
-     *  @returns {any[]}
-     */
-    values:() => {
-      let values = [];
+    return resp;
+  },
+  /*
+   * Returns all values for a given object or array as an array.
+   *
+   *  @returns {any[]}
+   */
+  values:(subject) => {
+    let values = [];
 
-      if(type(subject) === 'array') {
-        for(let i of subject){
-          values = values.concat(ob(i).values());
-        }
-      } else {
-        for(let k in subject) {
-          values.push(subject[k]);
-        };
+    if(type(subject) === 'array') {
+      for(let i of subject){
+        values = values.concat(ob.values(i));
       }
+    } else {
+      for(let k in subject) {
+        values.push(subject[k]);
+      };
+    }
 
-      return uniques(values);
-    },
-  };
+    return uniques(values);
+  },
 };
 
 if (typeof module !== 'undefined') {
--- tests.js
+++ tests.js
@@ -51,73 +51,73 @@ describe('Objob', () => {
 
   describe('keys', () => {
     it('should return all keys for an object', (done) => {
-      expect(ob(ob1).keys()).to.include.members(['name','age', 'weight']);
+      expect(ob.keys(ob1)).to.include.members(['name','age', 'weight']);
       done();
     });
     it('should return all keys for an array', (done) => {
-      expect(ob(obArr1).keys()).to.include.members(['name','age', 'weight','feet']);
+      expect(ob.keys(obArr1)).to.include.members(['name','age', 'weight','feet']);
       done();
     });
   });
 
   describe('values', () => {
     it('should return all values for an object', (done) => {
-      expect(ob(ob1).values()).to.include.members([ob1.name, ob1.age, ob1.weight]);
+      expect(ob.values(ob1)).to.include.members([ob1.name, ob1.age, ob1.weight]);
       done();
     });
     it('should return all values for an array', (done) => {
-      expect(ob(obArr1).values()).to.include.members([ob1.name, ob1.age, ob1.weight, ob2.feet]);
+      expect(ob.values(obArr1)).to.include.members([ob1.name, ob1.age, ob1.weight, ob2.feet]);
       done();
     });
   });
 
   describe('many', () => {
     it('should return the correct number of objects', (done) => {
-      expect(ob(ob1).many()).to.deep.equal([ob1,ob1]);
-      expect(ob(ob1).many(5)).to.deep.equal([ob1,ob1,ob1,ob1,ob1]);
+      expect(ob.many(ob1)).to.deep.equal([ob1,ob1]);
+      expect(ob.many(ob1, 5)).to.deep.equal([ob1,ob1,ob1,ob1,ob1]);
       done();
     });
 
     it('should return the array if an array', (done) => {
-      expect(ob(obArr1).many()).to.equal(obArr1);
+      expect(ob.many(obArr1)).to.equal(obArr1);
       done();
     });
   });
 
   describe('select', () => {
     it('should return the object only select the given keys', (done) => {
-      expect(ob(ob1).select(['name'])).to.deep.equal({name: ob1.name});
-      expect(ob(ob2).select(['name', 'age'])).to.deep.equal({name: ob2.name, age: ob2.age});
-      expect(ob(ob3).select(['eyes[].0.location'])).to.deep.equal({
+      expect(ob.select(ob1, ['name'])).to.deep.equal({name: ob1.name});
+      expect(ob.select(ob2,['name', 'age'])).to.deep.equal({name: ob2.name, age: ob2.age});
+      expect(ob.select(ob3, ['eyes[].0.location'])).to.deep.equal({
         eyes: [{location: 'left'}],
       });
       done();
     });
 
     it('should return the object only select the given keys using nested object', (done) => {
-      expect(ob(ob3).select(['body.feet'])).to.deep.equal({body: {feet: ob3.body.feet}});
+      expect(ob.select(ob3, ['body.feet'])).to.deep.equal({body: {feet: ob3.body.feet}});
       done();
     });
 
     it('should return the array only select the given keys using nested object', (done) => {
-      expect(ob([ob3, ob3]).select(['body.feet']))
+      expect(ob.select([ob3, ob3], ['body.feet']))
       .to.deep.equal([{body: {feet: ob3.body.feet}}, {body: {feet: ob3.body.feet}}]);
       done();
     });
 
     it('should return an array of objects only select the given keys', (done) => {
-      expect(ob(obArr1).select(['name'])).to.deep.equal([{name: ob1.name},{name: ob2.name}]);
+      expect(ob.select(obArr1, ['name'])).to.deep.equal([{name: ob1.name},{name: ob2.name}]);
       done();
     });
   });
 
   describe('deselect', () => {
     it('should return the object only deselect the given keys', (done) => {
-      expect(ob(ob1).deselect(['name'])).to.deep.equal({age: ob1.age, weight: ob1.weight});
-      expect(ob(ob2).deselect(['name', 'age'])).to.deep.equal({weight: ob2.weight, feet: ob2.feet});
+      expect(ob.deselect(ob1, ['name'])).to.deep.equal({age: ob1.age, weight: ob1.weight});
+      expect(ob.deselect(ob2, ['name', 'age'])).to.deep.equal({weight: ob2.weight, feet: ob2.feet});
       done();
 
-      expect(ob(ob3).deselect(['eyes[].0.location'])).to.deep.equal({
+      expect(ob.deselect(ob3, ['eyes[].0.location'])).to.deep.equal({
         name: ob3.name,
         feet: ob3.feet,
         body: {
@@ -130,7 +130,7 @@ describe('Objob', () => {
     });
 
     it('should return an array of objects only deselect the given keys', (done) => {
-      expect(ob(obArr1).deselect(['name', 'weight', 'feet']))
+      expect(ob.deselect(obArr1, ['name', 'weight', 'feet']))
       .to.deep.equal([{age: ob1.age}, {age: ob2.age}]);
 
       done();
@@ -139,7 +139,7 @@ describe('Objob', () => {
 
   describe('flatten', () => {
     it('should return the flattened object', (done) => {
-      expect(ob(ob3).flatten()).to.deep.equal({
+      expect(ob.flatten(ob3)).to.deep.equal({
         name: 'Bob',
         feet: 5,
         body: {
@@ -157,24 +157,24 @@ describe('Objob', () => {
         'eyes[].0.color': 'blue',
         'eyes[].1.color': 'red',
       });
-      expect(ob(ob1).flatten()).to.deep.equal(ob1);
+      expect(ob.flatten(ob1)).to.deep.equal(ob1);
       done();
     });
   });
 
   describe('expand', () => {
     it('should return the expanded object', (done) => {
-      expect(ob(ob(ob3).flatten()).expand()).to.deep.equal(ob3);
-      expect(ob(ob(ob2).flatten()).expand()).to.deep.equal(ob2);
-      expect(ob(ob(ob4).flatten()).expand()).to.deep.equal(ob4);
-      expect(ob(ob(obArr1).flatten()).expand()).to.deep.equal(obArr1);
+      expect(ob.expand(ob.flatten(ob3))).to.deep.equal(ob3);
+      expect(ob.expand(ob.flatten(ob2))).to.deep.equal(ob2);
+      expect(ob.expand(ob.flatten(ob4))).to.deep.equal(ob4);
+      expect(ob.expand(ob.flatten(obArr1))).to.deep.equal(obArr1);
       done();
     });
   });
 
   describe('Chaining', () => {
     it('should return many of an object after filtering select select', (done) => {
-      expect(ob(ob(ob1).select(['name', 'age'])).many(3))
+      expect(ob.many(ob.select(ob1, ['name', 'age']),3))
       .to.deep.equal([
         {age: ob1.age, name: ob1.name},
         {age: ob1.age, name: ob1.name},
